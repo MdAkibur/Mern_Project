8ef30a8451b46a347ee9a779857c306cdd7fa11e
Akib <akiburrahmankhan01@gmail.com>
2022-07-20 13:53:05 +0600

    full mobile responsive

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+const app = express();
+dotenv.config();
+
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use('/posts',postRoutes);
+
+/*app.get('/', (req, res) => {
+  res.send('Akib is the best');
+});*/
+
+
+
+const CONNECTION_URL = 'mongodb+srv://akiburproject:akiburproject123@cluster0.r9lfu9f.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
